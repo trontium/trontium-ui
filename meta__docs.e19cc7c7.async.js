aeabdf3d95280b0b22dd8813f97853014898f8b4
trontium <trontium@users.noreply.github.com>
2026-01-31 09:38:15 +0000

    deploy: 902114037d442bee1ac15d7286b44f1ae90a9fa4

---
+++ meta__docs.e19cc7c7.async.js
@@ -0,0 +1,28 @@
+"use strict";(self.webpackChunkdocs_site=self.webpackChunkdocs_site||[]).push([[904],{45038:function(t,a,n){n.r(a),n.d(a,{demos:function(){return d}});var e=n(96832),d={}},74653:function(t,a,n){n.r(a),n.d(a,{demos:function(){return d}});var e=n(96832),d={}},88779:function(t,a,n){n.r(a),n.d(a,{texts:function(){return e}});const e=[{value:"\u4F7F\u7528 npm \u6216 yarn \u5B89\u88C5",paraId:0,tocIndex:1},{value:`npm install @zzzzw/happy-ui
+`,paraId:1,tocIndex:1},{value:`yarn add @zzzzw/happy-ui
+`,paraId:2,tocIndex:1},{value:`import Alert from '@zzzzw/happy-ui/es/alert';
+// \u624B\u52A8\u6309\u9700\u52A0\u8F7D js
+import '@zzzzw/happy-ui/es/alert/style';
+
+// \u624B\u52A8\u6309\u9700\u52A0\u8F7D less
+
+ReactDOM.render(<Alert kind="warning">\u8FD9\u662F\u4E00\u6761\u8B66\u544A\u63D0\u793A</Alert>, mountNode);
+`,paraId:3,tocIndex:2},{value:"\u4F7F\u7528 ",paraId:4,tocIndex:3},{value:"babel-plugin-import ",paraId:4,tocIndex:3},{value:" \u4F18\u5316\u5F15\u5165\u65B9\u5F0F\uFF0C\u5982\u4E0B\uFF1A",paraId:4,tocIndex:3},{value:`import { Alert } from '@zzzzw/happy-ui';
+
+// \u4E0E\u4E0A\u8FF0\u793A\u4F8B\u7B49\u4EF7
+
+ReactDOM.render(<Alert kind="warning">\u8FD9\u662F\u4E00\u6761\u8B66\u544A\u63D0\u793A</Alert>, mountNode);
+`,paraId:5,tocIndex:3},{value:"\u5B89\u88C5 ",paraId:6,tocIndex:3},{value:"babel-plugin-import",paraId:6,tocIndex:3},{value:`yarn add babel-plugin-import --dev
+`,paraId:7,tocIndex:3},{value:"\u914D\u7F6E",paraId:8,tocIndex:3},{value:".babelrc",paraId:8,tocIndex:3},{value:" \u6216 ",paraId:8,tocIndex:3},{value:"babel-loader",paraId:8,tocIndex:3},{value:`{
+  "plugins": [
+    [
+      "import",
+      {
+        "libraryName": "@zzzzw/happy-ui",
+        "libraryDirectory": "esm", // default: lib
+        "style": true // or 'css'
+      }
+    ]
+  ]
+}
+`,paraId:9,tocIndex:3}]},67988:function(t,a,n){n.r(a),n.d(a,{texts:function(){return e}});const e=[]}}]);
